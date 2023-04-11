383a1d1e90c519d7f50b259e328d50e990e449f6
Madhukar B <114948509+Messionat45@users.noreply.github.com>
2023-04-11 18:38:47 +0530

    Update todo.js

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -4,11 +4,11 @@ const { all, markAsComplete, add, overdue, dueLater, dueToday } = todoList();
 
 describe("Todolist Test Suite", () => {
   beforeAll(() => {
-    //  add({
-    //  title: "Test todo",
-    // completed: false,
-    // duDate: new Date().toISOString().slice(0, 10),
-    //});
+    add({
+      title: "Test todo",
+      completed: false,
+      duDate: new Date().toISOString().slice(0, 10),
+    });
   });
   test("Should add new todo", () => {
     const todoItemsCount = all.length;
@@ -25,34 +25,68 @@ describe("Todolist Test Suite", () => {
     expect(all[0].completed).toBe(true);
   });
 
-  //overdue
+  //overdue new
   test("Should checks retrieval of overdue items.", () => {
-    let overdueList = overdue();
-
-    expect(
-      overdueList.every((event) => {
-        return event.dueDate < formattedDate(new Date());
-      })
-    ).toBe(true);
+    let overdueItemcount = overdue().length;
+    add({
+      title: "overdue todo ",
+      completed: false,
+      dueDate: new Date("2023-04-01").toISOString().split(0, 10),
+    });
+    let newOverdueItemCount = overdue().length;
+    expect(newOverdueItemCount).toBe(overdueItemcount + 1);
   });
 
-  // duetoday
+  //overdue
+  //test("Should checks retrieval of overdue items.", () => {--------------------------------
+  //let overdueList = overdue();
+
+  //expect(
+  //  overdueList.every((event) => {
+  //    return event.dueDate < formattedDate(new Date());
+  //   })
+  //  ).toBe(true);
+  // });------------------------------------------------------------
+
+  //due today new
   test("Should checks retrieval of due today items", () => {
-    let dueTodaylist = dueToday();
-    expect(
-      dueTodaylist.every((event) => {
-        return event.dueDate === formattedDate(new Date());
-      })
-    ).toBe(true);
+    let dueTodayItemCount = dueToday().length;
+    add({
+      title: "due today todo",
+      completed: false,
+      dueDate: new Date().toISOString().slice(0, 10),
+    });
+    let newDueTodayItemCount = dueToday().length;
+    expect(newDueTodayItemCount).toBe(dueTodayItemCount + 1);
   });
+  // duetoday
+  // test("Should checks retrieval of due today items", () => {--------------------------------
+  //   let dueTodaylist = dueToday();
+  //   expect(
+  //     dueTodaylist.every((event) => {
+  //       return event.dueDate === formattedDate(new Date());
+  //    })
+  //   ).toBe(true);
+  // });----------------------------------------------------------------
 
-  // duelater
+  //due later new
   test("Should checks retrieval of due later items", () => {
-    let dueLaterlist = dueLater();
-    expect(
-      dueLaterlist.every((event) => {
-        return event.dueDate > formattedDate(new Date());
-      })
-    ).toBe(true);
+    dueLaterItemCount = dueLater().length;
+    add({
+      title: "upoming todo",
+      completed: false,
+      dueDate: new Date("2023-05-01").toISOString().slice(0, 10),
+    });
+    newDueLaterItemCount = dueLater().length;
+    expect(newDueLaterItemCount).toBe(dueLaterItemCount + 1);
   });
+  // duelater
+  // test("Should checks retrieval of due later items", () => {-----------------------------------
+  // let dueLaterlist = dueLater();
+  //expect(
+  // dueLaterlist.every((event) => {
+  //  return event.dueDate > formattedDate(new Date());
+  //})
+  //).toBe(true);
+  //});---------------------------------------------------------------------
 });
