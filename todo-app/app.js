aed7b0076ff724bc838ef85e6e03ab85df036f8e
Madhukar Bondre <tejasbondre27@gmail.com>
2023-05-16 00:56:56 +0530

    todays final commit 15 may

--- todo-app/app.js
+++ todo-app/app.js
@@ -1,10 +1,14 @@
 const express = require("express");
+var csrf = require("csurf");
 const app = express();
 const { Todo } = require("./models");
 const bodyParser = require("body-parser");
+var cookieParser = require("cookie-parser");
 const path = require("path");
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser("shh! some secret string"));
+app.use(csrf({ cookie: true }));
 
 app.set("view engine", "ejs");
 
@@ -15,6 +19,7 @@ app.get("/", async (request, response) => {
   if (request.accepts("html")) {
     response.render("index", {
       allTodos,
+      csrfToken: request.csrfToken(),
     });
   } else {
     response.json({
