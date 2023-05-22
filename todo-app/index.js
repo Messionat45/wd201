f4f32c2994e9d99369dd52ca7f5f0c4df1fd90a4
Madhukar Bondre <tejasbondre27@gmail.com>
2023-05-22 13:57:29 +0530

    index.js commit port no

--- todo-app/index.js
+++ todo-app/index.js
@@ -1,5 +1,11 @@
 const app = require("./app");
 
-app.listen(3012, () => {
+app.listen(3013, () => {
   console.log("Started express server at port 3000");
 });
+
+// const app = require("./app");
+
+// app.listen(process.env.PORT || 3013, () => {
+//   console.log("Started express server at port 3000");
+// });
